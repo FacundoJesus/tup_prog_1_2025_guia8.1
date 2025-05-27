e7ef8abaee467b20070afaca79bc9e390dc097ab
FacuJesus <facundojesus10@hotmail.com>
2025-05-27 17:56:06 -0300

    Tenis

---
+++ Guia 8.1/Ejercicio4/Models/Servicio.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ejercicio4.Models
+{
+    internal class Servicio
+    {
+        private string jugador1;
+        private string jugador2;
+        private int setGanados1;
+        private int setGanados2;
+
+        public void RegistrarJugadores(string nombre1, string nombre2)
+        {
+            jugador1 = nombre1;
+            jugador2 = nombre2;
+        }
+        public void RegistrarResultadosSet(int resultado1, int resultado2)
+        {
+            if (resultado1 > resultado2)
+            {
+                setGanados1++;
+            }
+            else
+            {
+                setGanados2++;
+            }
+        }
+        public string DeterminarGanador()
+        {
+            string ganador = "";
+            if (setGanados1 > setGanados2)
+            {
+                ganador = jugador1;
+            }
+            else
+            {
+                ganador = jugador1;
+            }
+            return ganador;
+        }
+
+    }
+}
---
+++ Guia 8.1/Ejercicio4/Program.cs
@@ -0,0 +1,84 @@
+﻿using Ejercicio4.Models;
+
+namespace Ejercicio4
+{
+   
+    internal class Program
+    {
+        static Servicio servicio = new Servicio();
+
+        static int MostrarPantallaSolicitarOpcionMenu()
+        {
+            Console.Clear();
+            Console.WriteLine("---MENU---");
+            Console.WriteLine("1 - Registrar los nombres de los dos jugadores.");
+            Console.WriteLine("2 - Registrar los resultados de cada set de los jugadores.");
+            Console.WriteLine("3 - Mostrar el ganador.");
+            Console.WriteLine("-1 Para Salir");
+            int op = Convert.ToInt32(Console.ReadLine());
+            return op;
+        }
+        static void MostrarPantallaSolicitarNombreJugadores()
+        {
+            Console.Clear();
+            
+            Console.WriteLine($"Ingrese el nombre del 1º Jugador:");
+            string jugador1 = Console.ReadLine();
+            Console.WriteLine($"Ingrese el nombre del 2º Jugador:");
+            string jugador2 = Console.ReadLine();
+            servicio.RegistrarJugadores(jugador1, jugador2);
+            Console.WriteLine("\nPresione Enter para volver al Menú Principal.");
+            Console.ReadKey();
+
+        }
+        static void MostrarPantallaSolicitarResultadoSet()
+        {
+            for(int i = 0; i < 3; i++) {
+                Console.Clear();
+                Console.WriteLine($"Ingrese los Puntos del {i + 1}º Set del Jugador 1:");
+                int ptos_setJ1 = Convert.ToInt32(Console.ReadLine());
+                Console.WriteLine($"Ingrese los Puntos del {i + 1}º Set del Jugador 2:");
+                int ptos_setJ2 = Convert.ToInt32(Console.ReadLine());
+                servicio.RegistrarResultadosSet(ptos_setJ1, ptos_setJ2);
+            }
+            Console.WriteLine("\nPresione Enter para volver al Menú Principal.");
+            Console.ReadKey();
+
+        }
+        static void MostrarPantallaGanador()
+        {
+            string ganadorJuego = servicio.DeterminarGanador();
+            Console.Clear();
+            Console.WriteLine($"Ganador del Juego: {ganadorJuego}");
+            Console.WriteLine("\nPresione Enter para volver al Menú Principal.");
+            Console.ReadKey();
+        }
+
+        #region MENU
+        static void Main(string[] args)
+        {
+            int opcion = MostrarPantallaSolicitarOpcionMenu();
+            while (opcion != -1) {
+                switch(opcion) {
+                    case 1:
+                        MostrarPantallaSolicitarNombreJugadores();
+                        break;
+                    case 2:
+                        MostrarPantallaSolicitarResultadoSet();
+                        break;
+                    case 3:
+                        MostrarPantallaGanador();
+                        break;
+                    default:
+                        opcion = -1;
+                        break;
+                }
+                if (opcion != -1)
+                {
+                    opcion = MostrarPantallaSolicitarOpcionMenu();
+                }
+            }
+        }
+        #endregion 
+    }
+}
