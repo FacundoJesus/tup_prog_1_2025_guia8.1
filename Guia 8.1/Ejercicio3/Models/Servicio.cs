cac5cc540f0bc6ce23182b98113ce8bed1cafd5f
FacuJesus <facundojesus10@hotmail.com>
2025-05-27 17:55:54 -0300

    Lista ordenada

---
+++ Guia 8.1/Ejercicio3/Models/Servicio.cs
@@ -0,0 +1,74 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ejercicio3.Models
+{
+    internal class Servicio
+    {
+        public string nombre0;
+        public string nombre1;
+        public string nombre2;
+
+        public int numeroLibreta0;
+        public int numeroLibreta1;
+        public int numeroLibreta2;
+        private int orden = 0;
+
+        public void RegistrarNombreYNumeroLibreta(string nombre, int nroLibreta)
+        {
+            switch (orden)
+            {
+                case 0:
+                    nombre0 = nombre;
+                    numeroLibreta0 = nroLibreta;
+                    break;
+                case 1:
+                    if (nroLibreta < numeroLibreta0)
+                    {
+                        nombre1 = nombre0;
+                        numeroLibreta1 = numeroLibreta0;
+
+                        nombre0 = nombre;
+                        numeroLibreta0 = nroLibreta;
+                    }
+                    else
+                    {
+                        nombre1 = nombre;
+                        numeroLibreta1 = nroLibreta;
+                    }
+                    break;
+                case 2:
+                    if (nroLibreta < numeroLibreta0)
+                    {
+                        nombre2 = nombre1;
+                        numeroLibreta2 = numeroLibreta1;
+
+                        nombre1 = nombre0;
+                        numeroLibreta1 = numeroLibreta0;
+
+                        nombre0 = nombre;
+                        numeroLibreta0 = nroLibreta;
+                    }
+                    else
+                    if (nroLibreta < numeroLibreta1)
+                    {
+                        nombre2 = nombre1;
+                        numeroLibreta2 = numeroLibreta1;
+
+                        nombre1 = nombre;
+                        numeroLibreta1 = nroLibreta;
+                    }
+                    else
+                    {
+                        nombre2 = nombre;
+                        numeroLibreta2 = nroLibreta;
+                    }
+                    break;
+            }
+            orden++;
+        }
+    }
+}
---
+++ Guia 8.1/Ejercicio3/Program.cs
@@ -0,0 +1,68 @@
+﻿using Ejercicio3.Models;
+
+namespace Ejercicio3
+{
+
+    internal class Program
+    {
+        static Servicio servicio = new Servicio();
+
+        static int MostrarPantallaSolicitarOpcionMenu()
+        {
+            Console.Clear();
+            Console.WriteLine("---MENU---");
+            Console.WriteLine("1- Registrar las notas de los tres alumnos");
+            Console.WriteLine("2- Mostrar lista ordenada");
+            Console.WriteLine("-1 Para Salir");
+            int opcion = Convert.ToInt32(Console.ReadLine());
+            return opcion;
+        }
+        static void MostrarPantallaSolicitarAlumnos()
+        {
+            for (int i = 0; i < 3; i++) {
+                Console.Clear();
+                Console.WriteLine($"Ingrese el nombre del {i+1}º alumno:");
+                string name = Console.ReadLine();
+                Console.WriteLine($"Ingrese la nota del {i + 1}º alumno:");
+                int nota = Convert.ToInt32(Console.ReadLine());
+                servicio.RegistrarNombreYNumeroLibreta(name, nota);
+            }
+            Console.WriteLine("\nPresione Enter para volver al Menú Principal.");
+            Console.ReadKey();
+        }
+        static void MostrarPantallaMostrarListaOrdenada()
+        {
+            Console.Clear();
+            Console.WriteLine("---Lista Ordenada---");
+            Console.WriteLine($"Nombre: {servicio.nombre0} , Nota: {servicio.numeroLibreta0}");
+            Console.WriteLine($"Nombre: {servicio.nombre1} , Nota: {servicio.numeroLibreta1}");
+            Console.WriteLine($"Nombre: {servicio.nombre2} , Nota: {servicio.numeroLibreta2}");
+            Console.WriteLine("\nPresione Enter para volver al Menú Principal.");
+            Console.ReadKey();
+        }
+
+        static void Main(string[] args)
+        {
+            int opc = MostrarPantallaSolicitarOpcionMenu();
+            while (opc != -1)
+            {
+                switch(opc) {
+                    case 1:
+                        MostrarPantallaSolicitarAlumnos();
+                        break;
+                    case 2:
+                        MostrarPantallaMostrarListaOrdenada();
+                        break;
+                    default:
+                        opc = -1;
+                        break;
+                }
+                if (opc != -1)
+                {
+                    opc = MostrarPantallaSolicitarOpcionMenu();
+                }
+
+            }
+        }
+    }
+}
