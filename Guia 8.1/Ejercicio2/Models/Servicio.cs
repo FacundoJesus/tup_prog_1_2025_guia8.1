8179cb8bdc626f3250f8752773f70938d53e0156
FacuJesus <facundojesus10@hotmail.com>
2025-05-27 17:55:45 -0300

    Las niñas

---
+++ Guia 8.1/Ejercicio2/Models/Servicio.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ejercicio2.Models
+{
+    internal class Servicio
+    {
+        public int edad0;
+        public int edad1;
+        public int edad2;
+        public int edad3;
+        public double monto;
+        public double monto0;
+        public double monto1;
+        public double monto2;
+        public double monto3;
+        public double porcentaje0;
+        public double porcentaje1;
+        public double porcentaje2;
+        public double porcentaje3;
+
+        public void RegistrarMontoARepartir(double m)
+        {
+            monto = m;
+        }
+        public void RegistrarEdad(int edad, int nroNiña)
+        {
+            switch (nroNiña)
+            {
+                case 0:
+                    edad0 = edad;
+                    break;
+                case 1:
+                    edad1 = edad;
+                    break;
+                case 2:
+                    edad2 = edad;
+                    break;
+                case 3:
+                    edad3 = edad;
+                    break;
+            }
+
+        }
+        public void CalcularMontosYPorcentajesARepartir()
+        {
+
+            int sumaEdades = edad0 + edad1 + edad2 + edad3;
+
+            porcentaje0 = (edad0 * 1.0 / sumaEdades) * 100;
+            porcentaje1 = (edad1 * 1.0 / sumaEdades) * 100;
+            porcentaje2 = (edad2 * 1.0 / sumaEdades) * 100;
+            porcentaje3 = (edad3 * 1.0 / sumaEdades) * 100;
+
+            monto0 = (porcentaje0 * monto) / 100;
+            monto1 = (porcentaje1 * monto) / 100;
+            monto2 = (porcentaje2 * monto) / 100;
+            monto3 = (porcentaje3 * monto) / 100;
+
+        }
+    }
+}
---
+++ Guia 8.1/Ejercicio2/Program.cs
@@ -0,0 +1,81 @@
+﻿using Ejercicio2.Models;
+namespace Ejercicio2
+{
+
+    internal class Program
+    {
+        static Servicio servicio = new Servicio();
+
+        static int MostrarPantallaSolicitarOpcionMenu()
+        {
+            Console.Clear();
+            Console.WriteLine("---MENU---");
+            Console.WriteLine("1- Iniciar Monto a repartir");
+            Console.WriteLine("2- Solicitar Edad Por niña");
+            Console.WriteLine("3- Mostrar monto y porcentajes que corresponde a cada niña");
+            Console.WriteLine("-1 Para Salir del programa.");
+            int opc = Convert.ToInt32(Console.ReadLine());
+            return opc;
+        }
+        static void MostrarPantallaSolicitarMontoARepartir()
+        {
+            Console.Clear();
+            Console.WriteLine("Ingrese el Monto a repartir:");
+            double montoRepartir = Convert.ToDouble(Console.ReadLine());
+            servicio.RegistrarMontoARepartir(montoRepartir);
+            Console.WriteLine("Monto Registrado. Presione Enter para volver al menú principal.");
+            Console.ReadKey();
+
+        }
+        static void MostrarPantallaSolicitarEdadesNiñas()
+        {
+            for (int i = 0; i < 4; i++) {
+                Console.Clear();
+                Console.WriteLine($"Ingrese la edad de la {i + 1}º niña:");
+                int edadNiña = Convert.ToInt32(Console.ReadLine());
+                servicio.RegistrarEdad(edadNiña, i);
+                Console.Clear();
+            }
+            Console.WriteLine("Presione Enter para volver al menú principal.");
+            Console.ReadKey();
+        }
+        static void MostrarPantallaCalcularMostrarMontoYPorcentajePorNiña()
+        {
+            
+            Console.Clear();
+            servicio.CalcularMontosYPorcentajesARepartir();
+            Console.WriteLine($"Prc de Edad de la Niña 1: {servicio.porcentaje0:f2}%, Monto: {servicio.monto0:c2}");
+            Console.WriteLine($"Prc de Edad de la Niña 2: {servicio.porcentaje1:f2}%, Monto: {servicio.monto1:c2}");
+            Console.WriteLine($"Prc de Edad de la Niña 3: {servicio.porcentaje2:f2}%, Monto: {servicio.monto2:c2}");
+            Console.WriteLine($"Prc de Edad de la Niña 4: {servicio.porcentaje3:f2}%, Monto: {servicio.monto3:c2}");
+            Console.WriteLine("Presione Enter para volver al menú principal.");
+            Console.ReadKey();
+        }
+        static void Main(string[] args)
+        {
+            int opt = MostrarPantallaSolicitarOpcionMenu();
+            while (opt != -1)
+            {
+                switch(opt) {
+                    case 1:
+                        MostrarPantallaSolicitarMontoARepartir();
+                        break;
+                    case 2:
+                        MostrarPantallaSolicitarEdadesNiñas();
+                        break;
+                    case 3:
+                        MostrarPantallaCalcularMostrarMontoYPorcentajePorNiña();
+                        break;
+                    default:
+                        opt = -1;
+                        break;
+                }
+                if (opt != -1)
+                {
+                    opt = MostrarPantallaSolicitarOpcionMenu();
+                }
+
+            }
+        }
+    }
+}
