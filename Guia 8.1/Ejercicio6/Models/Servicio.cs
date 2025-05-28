55dfb0a33838e4d27ebdd7ed249423a19721c2f9
FacuJesus <facundojesus10@hotmail.com>
2025-05-28 17:28:55 -0300

    Encuesta

---
+++ Guia 8.1/Ejercicio6/Models/Servicio.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ejercicio6.Models
+{
+    internal class Servicio
+    {
+        #region Variables de la clase program
+        static int indecisos;
+        static int negativos;
+        static int positivos;
+        static int encuestados;
+        static double porcentajeIndecisos;
+        static double porcentajeNegativos;
+        static double porcentajePositivos;
+        #endregion
+
+        #region Métodos de proceso
+        public void RegistrarOpinion(int opinion)
+        {
+            switch (opinion)
+            {
+                case 0:
+                    positivos++;
+                    break;
+                case 1:
+                    negativos++;
+                    break;
+                case 2:
+                    indecisos++;
+                    break;
+                default:
+                    Console.WriteLine("Opinión no válida");
+                    break;
+            }
+            encuestados = positivos + negativos + indecisos;
+        }
+        public void ProcesarEncuesta()
+        {
+            if (encuestados > 0)
+            {
+                porcentajeIndecisos = (indecisos * 1.0 / encuestados) * 100;
+                porcentajeNegativos = (negativos * 1.0 / encuestados) * 100;
+                porcentajePositivos = (positivos * 1.0 / encuestados) * 100;
+                Console.WriteLine($"Porcentaje de Indecisos: {porcentajeIndecisos:f2}% / Cantidad de Indecisos: {indecisos}");
+                Console.WriteLine($"Porcentaje de Negativos: {porcentajeNegativos:f2}% / Cantidad de Negativos: {negativos}");
+                Console.WriteLine($"Porcentaje de Positivos: {porcentajePositivos:f2}% / Cantidad de Positivos: {positivos}");
+                Console.WriteLine($"Cantidad de Encuestados: {encuestados}");
+            }
+            else
+            {
+                Console.WriteLine("No hubo encuestados.");
+            }
+        }
+        #endregion
+    }
+}
---
+++ Guia 8.1/Ejercicio6/Program.cs
@@ -0,0 +1,80 @@
+﻿using Ejercicio6.Models;
+
+namespace Ejercicio6
+{
+    internal class Program
+    {
+        static Servicio servicio = new Servicio();
+
+        #region Métodos para imprimir en pantalla
+        static int MostrarPantallaSolicitarOpcionMenu()
+        {
+            Console.Clear();
+
+            Console.WriteLine("---MENU---");
+            Console.WriteLine("Bienvenido a la Encuesta");
+            Console.WriteLine("1- Registrar opinión");
+            Console.WriteLine("2- Procesar y mostrar resultados encuesta");
+            Console.WriteLine("-1 Para salir");
+            int opt = Convert.ToInt32(Console.ReadLine());
+            return opt;
+
+        }
+        static void MostrarPantallaRegistrarEncuesta()
+        {
+            Console.Clear();
+
+            Console.WriteLine("Ingrese la opinión:\n0 - Positivo\n1 - Negativo\n2 - Indeciso\n-1 Para Salir");
+            int opinion = Convert.ToInt32(Console.ReadLine());
+
+            while (opinion != -1)
+            {
+
+                servicio.RegistrarOpinion(opinion);
+
+                Console.Clear();
+                Console.WriteLine("Ingrese la opinión:\n0 - Positivo\n1 - Negativo\n2 - Indeciso\n-1 Para Salir");
+                opinion = Convert.ToInt32(Console.ReadLine());
+
+            }
+
+
+        }
+        static void MostrarPantallaProcesarMostrarResultadosEncuesta()
+        {
+            Console.Clear();
+            servicio.ProcesarEncuesta();
+            Console.WriteLine("\nPresione Enter para volver al Menú principal");
+            Console.ReadKey();
+        }
+        #endregion
+
+       
+        #region Menú consola
+        static void Main(string[] args)
+        {
+            int opcion = MostrarPantallaSolicitarOpcionMenu();
+            while (opcion != -1)
+            {
+                switch (opcion)
+                {
+                    case 1:
+                        MostrarPantallaRegistrarEncuesta();
+                        break;
+                    case 2:
+                        MostrarPantallaProcesarMostrarResultadosEncuesta();
+                        break;
+                    default:
+                        opcion = -1;
+                        break;
+                }
+                if (opcion != -1)
+                {
+                    opcion = MostrarPantallaSolicitarOpcionMenu();
+                }
+            }
+        }
+        #endregion 
+    }
+}
+
